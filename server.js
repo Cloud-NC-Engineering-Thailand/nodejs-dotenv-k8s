83eae273e3decf8b8d8a3c4cf76eebea3ae64614
Oamsan <oamsan@Oamsans-MacBook-Pro.local>
2022-12-19 15:44:13 +0700

    Initial here

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+require('dotenv').config()
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('DB Host: '+process.env.DB_HOST+', DB User: '+process.env.DB_USER+', DB Password: '+process.env.DB_PASSWORD);
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
